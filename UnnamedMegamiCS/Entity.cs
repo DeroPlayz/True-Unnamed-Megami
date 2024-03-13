73d7b4c45594e05793ec57ba536cdc165fe6f3ca
DeroPlayz <88916740+DeroPlayz@users.noreply.github.com>
2024-03-13 20:23:19 +0000

    Oh boy.

---
+++ UnnamedMegamiCS/Entity.cs
@@ -0,0 +1,13 @@
+class Entity{
+    String Name;
+    int Level;
+    double Cash;
+    String Arcana;
+
+    Entity(String Name, int Level, double Cash, String Arcana){
+        this.Name = Name;
+        this.Level = Level;
+        this.Cash = Cash;
+        this.Arcana = Arcana;
+    }
+}
