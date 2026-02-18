9c32d38f70607f0e4728e3f481190649bf26248a
Matthew <deroplayz@gmail.com>
2026-02-18 09:51:17 -0800

    Accidentally made an entire system to make it so the player choices for the Loop method that controls standard gameplay can be supplied via arguments, just to realize I need to sleep more because I literally didn't need that functionality. I'm writing this commit to ensure that if I ever need it, I can come back to this code and thank my dumbass past self.

--- src/Main.java
+++ src/Main.java
@@ -41,17 +41,17 @@ class Main{
         System.out.println(MafLib.CYAN + "2. Load Game");
         System.out.println(MafLib.BLACK + "3. Settings" + MafLib.RESET);
         Answer = MafLib.askInt("");
-        if(Answer < 1 || Answer > 3){
+        if (Answer < 1 || Answer > 3){
             ClearScreen();
             StartUp();
         }
-        else if(Answer == 1){
+        else if (Answer == 1){
             New();
         }
-        else if(Answer == 2){
+        else if (Answer == 2){
             Load();
         }
-        else if(Answer == 3){
+        else if (Answer == 3){
             Settings();
         }
     }
@@ -63,12 +63,12 @@ class Main{
 
     private static void Save(){
         int slot = MafLib.askInt(MafLib.GREEN + "Which slot would you like to save in?\n" + MafLib.RESET);
-        if(slot < 1 || slot > 10){
+        if (slot < 1 || slot > 10){
             ClearScreen();
             StartUp();
         }
         else{
-            if(new File("User/Save" + slot).exists()){
+            if (new File("User/Save" + slot).exists()){
                 MafLib.askString(MafLib.RED + "This slot contains existing save data. If you intend to overwrite it, press Enter to continue.\nIf you do not wish to overwrite the save, press CTRL+C (or CMD+C on Mac) to exit the game.\n" + MafLib.RESET);
             }
             FileOutputStream FOS;
@@ -92,7 +92,7 @@ class Main{
         try{
             SR = new FileInputStream("User/Save1");
             for(int i = 1; i <= 10; i++){
-                if(new File("User/Save" + i).exists()){
+                if (new File("User/Save" + i).exists()){
                     SR = new FileInputStream("User/Save" + i);
                     ObjectInputStream ReadFromSave = new ObjectInputStream(SR);
                     pr += "\n" + i + ". " + ReadFromSave.readObject();
@@ -106,7 +106,7 @@ class Main{
         //Asks the user to select a slot.
         int slot = MafLib.askInt(MafLib.CYAN + pr + MafLib.RESET + "\n");
         //If the slot chosen is invalid, repeat the question.
-        if(slot < 1 || slot > 10){
+        if (slot < 1 || slot > 10){
             ClearScreen();
             Load();
         }
@@ -127,11 +127,10 @@ class Main{
                 New();
             }
         }
-        Player.Attack(Skill.Melee);
     }
 
     private static void Load(int slot){
-        if(slot < 1 || slot > 10){
+        if (slot < 1 || slot > 10){
             ClearScreen();
             Load();
         }
@@ -154,7 +153,7 @@ class Main{
         System.out.println(MafLib.BLACK + "--- Settings ---");
         System.out.println("0. Go Back" + MafLib.RESET);
         Answer = MafLib.askInt("");
-        if(Answer == 0){
+        if (Answer == 0){
             ClearScreen();
             StartUp();
         }
@@ -166,8 +165,59 @@ class Main{
     }
 
     public static void Loop() {
-        System.out.println(Map.WORLD_MAP.toString());
-        System.out.println(Player.toString());
-        System.out.println(Stella.toString());
+        // System.out.println(Map.WORLD_MAP.toString());
+        // System.out.println(Player.toString());
+        // System.out.println(Stella.toString());
+        Answer = MafLib.askInt("What would you like to do?\n" + MafLib.GREEN + "1. Move\n" + MafLib.YELLOW + "2. View Map\n" + MafLib.BLUE + "3. Status\n" + MafLib.BLACK + "4. Settings\n" + MafLib.RESET);
+        if (Answer == 1){
+            Answer = MafLib.askInt("Which direction?\n1. Up\n2. Down\n3. Left\n4. Right\n");
+            if (Answer == 1){Player.setZ(Player.Z - 1);} // Up
+            if (Answer == 2){Player.setZ(Player.Z + 1);} // Down
+            // P.S. I now understand why lower Y is used for "up" sometimes.
+            if (Answer == 3){Player.setX(Player.Z - 1);} // Left
+            if (Answer == 4){Player.setX(Player.Z + 1);} // Right
+        }
+        else if (Answer == 2){
+            System.out.println(Map.WORLD_MAP.toString());
+        }
+        else if (Answer == 3){
+            Answer = MafLib.askInt("What would you like to check?\n" + MafLib.BLUE + "1. Party\n" + MafLib.GREEN + "2. Inventory\n" + MafLib.RED + "3. Compendium\n" + MafLib.BLACK + "4. Nevermind, go back.\n" + MafLib.RESET);
+            if (Answer == 4){
+                ClearScreen();
+                Loop(new int[]{3});
+            }
+        }
+        Loop();
+    }
+
+    public static void Loop(int[] args){
+        System.out.println("What would you like to do?\n" + MafLib.GREEN + "1. Move\n" + MafLib.YELLOW + "2. View Map\n" + MafLib.BLUE + "3. Status\n" + MafLib.BLACK + "4. Settings\n" + MafLib.RESET);
+        System.out.println(args[0]);
+        if (args[0] == 1){
+            if (args.length < 2){args[1] = MafLib.askInt("Which direction?\n1. Up\n2. Down\n3. Left\n4. Right\n");}
+            else{
+                System.out.println("Which direction?\n1. Up\n2. Down\n3. Left\n4. Right\n");
+                // System.out.println(args[1]);
+            }
+            if (args[1] == 1){Player.setZ(Player.Z - 1);} // Up
+            if (args[1] == 2){Player.setZ(Player.Z + 1);} // Down
+            // P.S. I now understand why lower Y is used for "up" sometimes.
+            if (args[1] == 3){Player.setX(Player.Z - 1);} // Left
+            if (args[1] == 4){Player.setX(Player.Z + 1);} // Right
+        }
+        else if (args[0] == 2){
+            System.out.println(args[0]);
+            System.out.println(Map.WORLD_MAP.toString());
+        }
+        else if (args[0] == 3){
+            if (args.length < 2){args[1] = MafLib.askInt("What would you like to check?\n" + MafLib.BLUE + "1. Party\n" + MafLib.GREEN + "2. Inventory\n" + MafLib.RED + "3. Compendium\n" + MafLib.BLACK + "4. Nevermind, go back.\n" + MafLib.RESET);}
+            else{
+                System.out.println("What would you like to check?\n" + MafLib.BLUE + "1. Party\n" + MafLib.GREEN + "2. Inventory\n" + MafLib.RED + "3. Compendium\n" + MafLib.BLACK + "4. Nevermind, go back.\n" + MafLib.RESET);
+            }
+            if (args[1] == 4){
+                ClearScreen();
+                Loop(new int[]{3});
+            }
+        }
     }
 }
