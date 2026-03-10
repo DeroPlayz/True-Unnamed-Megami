14af6ae90e230f5c177ec1fc02301d4bf0168923
Matthew <deroplayz@gmail.com>
2026-03-10 10:50:25 -0700

    moved unneeded custom catch to correct area

--- src/main/java/dero/unnamed_megami/MafLib.java
+++ src/main/java/dero/unnamed_megami/MafLib.java
@@ -99,13 +99,8 @@ public class MafLib{
             if (String.valueOf(message.charAt(i)).equals("|")){current_speed = text_speed * 2;}
             try {
                 Thread.sleep(current_speed);
-            } catch (InterruptedException | IllegalArgumentException e) {
-                System.out.println("Sorry! Your text speed is invalid.\n"
-                + "Please check your settings to ensure there are no negative numbers\n"
-                + "or values beyond Java's integer limit, which is " + Integer.MAX_VALUE + "\n");
-                Main.menu_text_speed = 100;
-                Main.story_text_speed = 100;
-                // e.printStackTrace();
+            } catch (InterruptedException e) {
+                e.printStackTrace();
             }
             if (!String.valueOf(message.charAt(i)).equals("|")){
                 System.out.print(message.charAt(i));}
