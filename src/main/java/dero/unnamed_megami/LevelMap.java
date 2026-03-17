dd6037aba88cdbcd49c28b421ba20966626e2b88
Matthew <deroplayz@gmail.com>
2026-03-17 11:15:14 -0700

    Figured out levels! This took SO fucking long....

--- src/main/java/dero/unnamed_megami/LevelMap.java
+++ src/main/java/dero/unnamed_megami/LevelMap.java
@@ -2,10 +2,15 @@ package dero.unnamed_megami;
 
 import static dero.unnamed_megami.Tile.*;
 
+import java.util.HashMap;
+import java.util.Map;
+
 public class LevelMap {
+    public String ID;
     public Tile[][] Bounds;
 
-    public LevelMap(Tile[][] Bounds){
+    public LevelMap(String ID, Tile[][] Bounds){
+        this.ID = ID;
         this.Bounds = Bounds;
     }
 
@@ -13,15 +18,42 @@ public class LevelMap {
         String s = "";
         for(int i = 0; i < Bounds.length; i++){
             for(int j = 0; j < Bounds[i].length; j++){
-                s += Bounds[i][j].toString() + " ";
+                if(Main.User.X_Position == j && Main.User.Y_Position == i){
+                    s += "U";
+                }
+                else{s += Bounds[i][j].toString();}
+                s += " ";
             }
-            s += "\n";
+            if(i != Bounds.length - 1){s += "\n";}
         }
         return s;
     }
 
-    public static final LevelMap Overworld = new LevelMap(new Tile[][]{
-        {Grass, Dirt, Dirt, Grass, Dirt, Grass, Dirt, Dirt, Dirt, Grass},
+    public static final LevelMap UserBedroom = new LevelMap("User's Bedroom", new Tile[][]{
+        {Floor, Floor, Floor, BedLeft, BedRight},
+        {Floor, Floor, Floor, Floor, Floor},
+        {Floor, Floor, Floor, Floor, Floor},
+        {Floor, Floor, Floor, Floor, Floor},
+        {Floor, Floor, Door_User_Bedroom_To_User_House_Hallway, Floor, Floor},
+    });
+    public static final LevelMap UserHouse = new LevelMap("User's House", new Tile[][]{
+        {Door_User_House_Hallway_To_User_Bedroom, Floor, Blank},
+        {Blank, Floor, Placeholder},
+        {Blank, Floor},
+        {Blank, Floor},
+        {Blank, Floor},
+        {Blank, Floor, Floor, Floor},
+        {Blank, Floor},
+        {Placeholder, Floor},
+    });
+
+    public static final LevelMap Overworld = new LevelMap("Overworld", new Tile[][]{
+        {Grass, Dirt, Floor, Grass, Dirt, Grass, Dirt, Dirt, Dirt, Grass},
         {Dirt, Grass, Grass, Dirt, Grass, Dirt, Grass, Grass, Grass, Dirt}
     });
+
+    public static HashMap<Integer, LevelMap> Maps = new HashMap<>(Map.of(
+        1, UserBedroom,
+        2, UserHouse
+    ));
 }
--- src/main/java/dero/unnamed_megami/Tile.java
+++ src/main/java/dero/unnamed_megami/Tile.java
@@ -1,31 +1,71 @@
 package dero.unnamed_megami;
 
-import java.util.ArrayList;
-
 public class Tile {
     public char Icon;
     public String Formatting = "";
     public int EncounterChance; // Out of 10;
-    public int Region;
-    public ArrayList<String> SpecialData;
 
-    public Tile(char Icon, String Formatting, int EncounterChance, int Region){
+    public int Destination;
+    public int XPos;
+    public int YPos;
+
+    public Tile(char Icon, String Formatting, int EncounterChance){
         this.Icon = Icon;
         this.Formatting = Formatting;
         this.EncounterChance = EncounterChance;
-        this.Region = Region;
     }
 
-    public Tile(char Icon, int EncounterChance, int Region){
+    public Tile(char Icon, String Formatting, int Destination, int XPos, int YPos){
+        this.Icon = Icon;
+        this.Formatting = Formatting;
+        this.EncounterChance = 0;
+        this.Destination = Destination;
+        this.XPos = XPos;
+        this.YPos = YPos;
+    }
+
+    public Tile(char Icon, int EncounterChance){
         this.Icon = Icon;
         this.EncounterChance = EncounterChance;
-        this.Region = Region;
     }
 
-    public static final Tile Grass = new Tile('G', MafLib.GREEN, 4, 0);
-    public static final Tile Dirt = new Tile('D', MafLib.YELLOW, 4, 0);
+    //Special Tiles
+    public static final Tile Blank = new Tile(' ', 0);
+    public static final Tile Placeholder = new Tile('?', MafLib.MAGENTA + MafLib.BOLD + MafLib.UNDERLINE, 0);
 
+    //Regular Tiles
+    public static final Tile Grass = new Tile('G', MafLib.GREEN, 2);
+    public static final Tile Dirt = new Tile('D', MafLib.YELLOW, 4);
+    public static final Tile Floor = new Tile('■', 4);
+    public static final Tile BedLeft = new Tile('b', MafLib.CYAN, 0);
+    public static final Tile BedRight = new Tile('d', MafLib.CYAN, 0);
+
+    //Doors
+    public static final Tile Door_User_Bedroom_To_User_House_Hallway = new Tile(
+        'D', MafLib.UNDERLINE,
+        2, 0, 0);
+    public static final Tile Door_User_House_Hallway_To_User_Bedroom = new Tile(
+        'D', MafLib.UNDERLINE,
+        1, 2, 4);
+    
+    public static final Tile[] Uncrossable_Tiles = {Blank, BedLeft, BedRight};
+
+    public boolean isCrossable(){
+        for(int i = 0; i < Uncrossable_Tiles.length; i++){
+            if (Uncrossable_Tiles[i].Icon == Icon
+            &&  Uncrossable_Tiles[i].Formatting == Formatting
+            &&  Uncrossable_Tiles[i].EncounterChance == EncounterChance
+            ){
+                return false;
+            }
+        }
+        return true;
+    }
     @Override public String toString(){
         return Formatting + Icon + MafLib.RESET; 
+    }
+
+    public String Formatted(){
+        return "Icon: " + Formatting + Icon + MafLib.RESET + "\nLeads to " + Destination + " at X-" + XPos + ", Y-" + YPos + "\n"; 
     }
 }
